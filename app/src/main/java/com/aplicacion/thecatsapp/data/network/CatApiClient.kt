cde5cc44cd67fde44acf623a7be46982b7a36c5d
Sebastian gallego jaramillo <93806822+sebasgallego@users.noreply.github.com>
2023-05-09 00:22:23 -0500

    Finished challenge

--- app/src/main/java/com/aplicacion/thecatsapp/data/network/CatApiClient.kt
+++ app/src/main/java/com/aplicacion/thecatsapp/data/network/CatApiClient.kt
@@ -4,8 +4,10 @@ import com.aplicacion.thecatsapp.data.model.Cat
 import com.aplicacion.thecatsapp.utils.GlobalsVar.GET_BREEDS
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Headers
 
 interface CatApiClient {
     @GET(GET_BREEDS)
+    @Headers("x-api-key: bda53789-d59e-46cd-9bc4-2936630fde39")
     suspend fun getCats(): Response<ArrayList<Cat>>
 }
